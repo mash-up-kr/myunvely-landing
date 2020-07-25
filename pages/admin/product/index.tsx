5961c3a2300456f7c877e218842690a58ee107e4
dev.park <skwjdgn2@gmail.com>
2020-07-25 21:36:59 +0900

    ADD esLint, pretter

--- pages/admin/product/index.tsx
+++ pages/admin/product/index.tsx
@@ -1,47 +1,91 @@
-import React from "react";
-import { Table } from "antd";
+import React from 'react';
+import { Table } from 'antd';
 
-import AdminLayout from "components/AdminLayout";
+import AdminLayout from 'components/AdminLayout';
 
 const AdminProductListPage = () => {
-  return (
-    <AdminLayout>
-      <Table dataSource={dataSource} columns={columns} />
-    </AdminLayout>
-  );
+	return (
+		<AdminLayout>
+			<Table dataSource={dataSource} columns={columns} />
+		</AdminLayout>
+	);
 };
 
 export default AdminProductListPage;
 
 const dataSource = [
-  {
-    key: "1",
-    name: "Mike",
-    age: 32,
-    address: "10 Downing Street",
-  },
-  {
-    key: "2",
-    name: "John",
-    age: 42,
-    address: "10 Downing Street",
-  },
+	{
+		key: '1',
+		category: '면도기',
+		name: '면도기01',
+		description: '짱짱 좋은 면도기',
+		cycle: 40,
+		imageUrl:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Maquinilla_de_afeitar_desechable.JPG/250px-Maquinilla_de_afeitar_desechable.JPG',
+		pirce: 1500,
+	},
+	{
+		key: '2',
+		category: '면도기',
+		name: '면도기01',
+		description: '짱짱 좋은 면도기',
+		cycle: 40,
+		imageUrl:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Maquinilla_de_afeitar_desechable.JPG/250px-Maquinilla_de_afeitar_desechable.JPG',
+		pirce: 1500,
+	},
+	{
+		key: '3',
+		category: '면도기',
+		name: '면도기01',
+		description: '짱짱 좋은 면도기',
+		cycle: 40,
+		imageUrl:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Maquinilla_de_afeitar_desechable.JPG/250px-Maquinilla_de_afeitar_desechable.JPG',
+		pirce: 1500,
+	},
+	{
+		key: '4',
+		category: '면도기',
+		name: '면도기01',
+		description: '짱짱 좋은 면도기',
+		cycle: 40,
+		imageUrl:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Maquinilla_de_afeitar_desechable.JPG/250px-Maquinilla_de_afeitar_desechable.JPG',
+		pirce: 1500,
+	},
 ];
 
 const columns = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Age",
-    dataIndex: "age",
-    key: "age",
-  },
-  {
-    title: "Address",
-    dataIndex: "address",
-    key: "address",
-  },
+	{
+		title: '이미지',
+		dataIndex: 'imageUrl',
+		key: 'imageUrl',
+		render: (imageUrl: string) => <img alt={imageUrl} src={imageUrl} />,
+	},
+	{
+		title: '카테고리',
+		dataIndex: 'category',
+		key: 'category',
+	},
+	{
+		title: '제품명',
+		dataIndex: 'name',
+		key: 'name',
+	},
+	{
+		title: '설명',
+		dataIndex: 'description',
+		key: 'description',
+	},
+	{
+		title: '주기',
+		dataIndex: 'cycle',
+		key: 'cycle',
+	},
+	{
+		title: '가격',
+		dataIndex: 'pirce',
+		key: 'pirce',
+	},
 ];
